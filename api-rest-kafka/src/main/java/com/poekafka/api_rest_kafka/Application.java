838747a68a03d415cda3ef678d4f93a1fecb69ba
Fábio Signorini <95833161+FabioSigF@users.noreply.github.com>
2024-10-21 18:01:46 -0300

    configuration change

--- api-rest-kafka/src/main/java/com/poekafka/api_rest_kafka/Application.java
+++ api-rest-kafka/src/main/java/com/poekafka/api_rest_kafka/Application.java
@@ -1,8 +1,11 @@
 package com.poekafka.api_rest_kafka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @EnableKafka
 @SpringBootApplication
@@ -10,6 +13,13 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("api", "hello");
+		};
 	}
 
 }
